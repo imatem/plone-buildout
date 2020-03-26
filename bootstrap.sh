d9f5aba23d2a2a00803115cdae1df85f3eb0cf19
Gilo <gil.cano@gmail.com>
2020-03-25 22:40:51 -0600

    update to python 3

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@
 
 # see https://community.plone.org/t/not-using-bootstrap-py-as-default/620
 rm -r ./lib ./include ./bin
-virtualenv-2.7 --no-setuptools --clear .
+virtualenv-3.7 .
 ./bin/pip install -r requirements.txt
 ./bin/buildout
